de944144ec60c1e1899eaaebf50cf04d678c7e34
Stefan Arvidsson <stearv@student.chalmers.se>
2012-09-24 23:30:58 +0200

    Removed the "login" GUI proposal and added Exit button, also added code to make an ExtandableList

--- workspace/AppsolutDistribute/gen/com/example/appsolutdistribute/R.java
+++ workspace/AppsolutDistribute/gen/com/example/appsolutdistribute/R.java
@@ -24,15 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080000;
-        public static final int editText1=0x7f080001;
-        public static final int editText3=0x7f080004;
-        public static final int serverAddress=0x7f080003;
-        public static final int textView1=0x7f080006;
-        public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080007;
-        public static final int textView4=0x7f080008;
+        public static final int exitButton=0x7f080000;
+        public static final int mainExpandableListView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,18 +35,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int connectToServer=0x7f04000c;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int text_Connect=0x7f040006;
-        public static final int text_Exit=0x7f040007;
-        public static final int text_Password=0x7f040009;
-        public static final int text_SelectServerToConnectTo=0x7f040005;
-        public static final int text_Submit=0x7f04000a;
-        public static final int text_UserName=0x7f04000b;
-        public static final int text_globalServerIP=0x7f040008;
-        public static final int title_ConnectToServer=0x7f040004;
-        public static final int title_activity_main=0x7f040003;
+        public static final int connectToServer=0x7f040005;
+        public static final int main_no_items=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int text_Exit=0x7f040004;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- workspace/AppsolutDistribute/src/com/example/appsolutdistribute/MainActivity.java
+++ workspace/AppsolutDistribute/src/com/example/appsolutdistribute/MainActivity.java
@@ -2,14 +2,74 @@ package com.example.appsolutdistribute;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.ViewGroup;
 
-public class MainActivity extends Activity {
+import android.view.View;
+
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.TextView;
+
+import android.graphics.Color;
+
+
+
+
+
+
+public class MainActivity extends Activity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //added by Stefan Arvidsson--------------------------------------------
+        Button closebutton = (Button)this.findViewById(R.id.exitButton);
+        closebutton.setOnClickListener(new OnClickListener(){
+           // @Override 
+            public void onClick(View v) { 
+                // TODO Auto-generated method stub 
+                finish(); 
+                System.exit(0); 
+            } 
+        });
+        
+        // For expandable list
+        ExpandableListView mainList = (ExpandableListView)this.findViewById(R.id.mainExpandableListView); 
+        ExpandableListAdapter mainListAdapter = new MainExpandableListAdapter();
+        mainList.setAdapter(mainListAdapter);
+        
+        mainList.setOnGroupClickListener( new OnGroupClickListener(){
+        	//@Override
+        	public boolean onGroupClick(ExpandableListView arg0, View arg1, int groupPosition,long arg3){
+        		if(groupPosition ==5){
+        			
+        		}
+        		return false;
+        	}
+        });
+        
+        
+        mainList.setOnChildClickListener(new ExpandableListView.OnChildClickListener(){
+        	public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id){
+        		if(groupPosition == 0 && childPosition == 0){
+        			
+        		}
+        		
+        		return false;
+        	}
+        });
+        //----------------------------------------------------------------------
+        
     }
 
     @Override
@@ -18,4 +78,98 @@ public class MainActivity extends Activity {
         return true;
     }
     
+
+    
+    /**
+     * An adapter to get the expandablelistview going.
+     * @author Stefan
+     *
+     */
+    private class MainExpandableListAdapter extends BaseExpandableListAdapter {
+
+    	private String[] groups = {"Parent1","Parent2","Parent3"};
+    	private String[][] children ={{"Child1"},{"Child2"},{"Child3"},{"Child4"},{"Child5"}};
+    	
+    	public Object getChild(int groupPosition, int childPosition) {
+    		// TODO Auto-generated method stub
+    		//return null;
+    		return children[groupPosition][childPosition];
+    	}
+    	public long getChildId(int groupPosition, int childPosition) {
+    		// TODO Auto-generated method stub
+    		//return 0;
+    		return childPosition;
+    	}
+    	public View getChildView(int groupPosition, int childPosition,
+    			boolean isLastChild, View convertView, ViewGroup parent) {
+    		// TODO Auto-generated method stub
+    		//return null;
+    		TextView textView = getGenericView();
+    		textView.setText(getChild(groupPosition,childPosition).toString());
+    		
+    		return textView;
+    	}
+    	public int getChildrenCount(int groupPosition) {
+    		// TODO Auto-generated method stub
+    		//return 0;
+    		int i = 0;
+    		try{
+    			i = children[groupPosition].length;
+    		}catch(Exception e){}
+    		return i;
+    	}
+    	
+    	public TextView getGenericView(){
+    		// layout parameters for the ExpandableListView
+    		AbsListView.LayoutParams lp = new AbsListView.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,64);
+    		TextView textView = new TextView(MainActivity.this);
+    		textView.setLayoutParams(lp);
+    		// center the text vertically
+    		textView.setGravity(Gravity.CENTER_VERTICAL|Gravity.LEFT);
+    		textView.setTextColor(Color.RED);
+    		// Set the text starting position
+    		textView.setPadding(36, 0, 0, 0);
+    		return textView;
+    		
+    	}
+    	
+    	public Object getGroup(int groupPosition) {
+    		// TODO Auto-generated method stub
+    		//return null;
+    		return groupPosition;
+    	}
+    	public int getGroupCount() {
+    		// TODO Auto-generated method stub
+    		return 0;
+    	}
+    	public long getGroupId(int groupPosition) {
+    		// TODO Auto-generated method stub
+    		return 0;
+    	}
+    	public View getGroupView(int groupPosition, boolean isExpanded,
+    			View convertView, ViewGroup parent) {
+    		// TODO Auto-generated method stub
+    		//return null;
+    		TextView textView = getGenericView();
+    		textView.setText(getGroup(groupPosition).toString());
+    		return textView;
+    	}
+    	public boolean hasStableIds() {
+    		// TODO Auto-generated method stub
+    		//return false;
+    		return true;
+    	}
+    	public boolean isChildSelectable(int groupPosition, int childPosition) {
+    		// TODO Auto-generated method stub
+    		//return false;
+    		return true;
+    	}
+    	
+
+    }
+    
 }
+
+
+
+
